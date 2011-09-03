faaf9731a34aafd2a38782b9016df1971fcb3798
Jack Bowman <open3141@gmail.com>
2011-09-03 01:11:57 +0000

    initial commit to github

---
+++ unhosted/config.js
@@ -0,0 +1,13 @@
+var appBaseUrl = 'http://todo.jack-bowman.com';
+//var installationType = 'flower';
+
+var config = {
+	appUrl: appBaseUrl + '/',
+	doUrl: appBaseUrl + '/unhosted/do.php',
+	loginUrl: appBaseUrl + '/unhosted/login.html',
+	//registerUrl: appBaseUrl + '/unhosted/register.html',
+	callbackUrl: appBaseUrl + '/unhosted/callback.html',
+	clientId: 'todo.jack-bowman.com',
+	dataScope: 'jacktodoapp',
+	homeDomain: 'todo.jack-bowman.com'
+}
