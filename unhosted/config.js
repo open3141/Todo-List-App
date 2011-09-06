cfc70b1f639a125ac146106ea2d08d7403f1103f
Michiel de Jong <michiel@unhosted.org>
2011-09-06 15:38:55 +0000

    get login working on relative url

--- unhosted/config.js
+++ unhosted/config.js
@@ -1,7 +1,8 @@
 var config = {
 	doUrl: 'unhosted/do.php',
 	loginUrl: 'unhosted/login.html',
-	callbackUrl: 'unhosted/callback.html',
+	callbackUrlRelChars: 10, // length of 'login.html'
+	callbackUrl: 'callback.html',
 	clientId: 'todo.jack-bowman.com',
 	dataScope: 'jacktodoapp',
 	homeDomain: 'todo.jack-bowman.com'
